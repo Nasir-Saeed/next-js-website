049475d8e62e52d73109977193e7a044c3ea49c2
Nasir Saeed <saeednasir483@gmail.com>
2024-05-16 13:28:28 +0500

    update files

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -15,16 +15,16 @@ interface Service {
 }
 
 function Card() {
-
-  const featuredServices = serviceDetail.services.filter((service:Service) => service.isFeatured);
-
+  const featuredServices = serviceDetail.services.filter(
+    (service: Service) => service.isFeatured
+  );
 
   return (
     <div className="py-12 bg-gray-900">
       <div>
         <div className="text-center">
           <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
-            Featured Services
+            Explore My Featured Services
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
             Hiring Me
@@ -33,11 +33,18 @@ function Card() {
       </div>
       <div className="mt-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-          {featuredServices.map((service:Service) => (
-            <div key={service.id} className="flex justify-cenetr">
+          {featuredServices.map((service: Service) => (
+            <div key={service.id} className="flex justify-center">
               <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
                 <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
-                  
+                  <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+                    {service.title}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
+                    {service.description}
+                  </p>
+                  <Link href={`/`} className="text-black">View More</Link>
+                  {/* <Link href={`/services/${service.slug}`}>View More</Link> */}
                 </div>
               </BackgroundGradient>
             </div>
@@ -46,7 +53,7 @@ function Card() {
       </div>
       <div className="mt-20 text-center">
         <Link
-          href={"/"}
+          href={"https://nasir-saeed-portfolio.netlify.app/img/cv/resume.pdf"}
           className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
         >
           Contact Me
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-  ProductItem,
-} from "@/components/ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
@@ -24,12 +19,14 @@ function Navbar({ className }: { className?: string }) {
             item="Home"
           ></MenuItem>
         </Link>
-        <MenuItem setActive={setActive} active={active} item="Our Courses">
+        <MenuItem setActive={setActive} active={active} item="Our Services">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/">Web Development</HoveredLink>
-            <HoveredLink href="/">Interface Design</HoveredLink>
-            <HoveredLink href="/">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/">Branding</HoveredLink>
+            <HoveredLink href="/">Website development</HoveredLink>
+            <HoveredLink href="/">Corporate website</HoveredLink>
+            <HoveredLink href="/">Startup websites</HoveredLink>
+            <HoveredLink href="/">Landings and promo sites</HoveredLink>
+            <HoveredLink href="/">E-commerce</HoveredLink>
+            <HoveredLink href="/">Wordpress development</HoveredLink>
           </div>
         </MenuItem>
       </Menu>
